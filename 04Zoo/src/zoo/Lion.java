4444b467d384f3e189279e7078dca62075c91886
Mirco <gavamirco@gmail.com>
2025-09-16 11:03:36 +0200

    Add files via upload

---
+++ 04Zoo/src/zoo/Lion.java
@@ -0,0 +1,25 @@
+package zoo;
+
+public class Lion extends Mammal implements Feedable{
+
+    public Lion(String name, int age, boolean hasFur) {
+        super(name, age, hasFur);
+    }
+
+    @Override
+    public void eat(String food)
+    {
+
+    }
+
+    @Override
+    public String makeSound() {
+        return "roar";
+    }
+
+    @Override
+    public String toString() {
+        return "Lion{" +
+                "\n name= " + getName() + "\n age= " + getAge() +  "\n hasFur= "+ isHasFur() +"}";
+    }
+}
---
+++ 04Zoo/src/zoo/Parrot.java
@@ -0,0 +1,25 @@
+package zoo;
+
+public class Parrot extends Animal implements Feedable{
+
+    public Parrot(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void eat(String food)
+    {
+
+    }
+
+    @Override
+    public String makeSound() {
+        return "gugugu";
+    }
+
+    @Override
+    public String toString() {
+        return "Parrot{" +
+                "\n name= " + getName() + "\n age= " + getAge() +"}";
+    }
+}
